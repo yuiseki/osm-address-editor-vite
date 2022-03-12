5a9aa703580b21b576f8ac01687863d7e072457e
yuiseki <yuiseki@gmail.com>
2022-03-12 16:19:41 +0900

    try to use openmaptiles

--- src/App.tsx
+++ src/App.tsx
@@ -83,6 +83,10 @@ const layerStyleFill: LayerProps = {
 function App() {
   const mapRef = useRef<MapRef>(null);
 
+  const [styleUrl, setStyleUrl] = useState(
+    "https://openmaptiles.github.io/positron-gl-style/style-cdn.json"
+  );
+
   const [viewState, setViewState] = useState<ViewState>();
   const debouncedViewState = useDebounce<ViewState>(viewState, 1000);
 
@@ -110,6 +114,15 @@ function App() {
   //
   useEffect(() => {
     setTimeout(() => {
+      // change style by hostname
+      if (
+        window.location.hostname === "localhost" ||
+        window.location.hostname === "127.0.0.1"
+      ) {
+        setStyleUrl(
+          "https://raw.githubusercontent.com/geoloniamaps/basic/gh-pages/style.json"
+        );
+      }
       // trigger geolocate if map hash is /0/0
       console.log(window.location.hash);
       if (!window.location.hash.endsWith("/0/0")) {
@@ -311,7 +324,7 @@ function App() {
           cursor={cursor}
           mapLib={maplibregl}
           style={{ width: "100%", height: "100%" }}
-          mapStyle="https://raw.githubusercontent.com/geoloniamaps/basic/gh-pages/style.json"
+          mapStyle={styleUrl}
         >
           <div
             className="fa-2xl"
