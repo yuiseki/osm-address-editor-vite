3c45f81740ad8d5f26f5095f62df11c1a001d6eb
yuiseki <yuiseki@gmail.com>
2022-03-19 15:23:58 +0900

    FIX: 9 tweet button working

--- src/components/TweetButton/index.tsx
+++ src/components/TweetButton/index.tsx
@@ -1,10 +1,15 @@
+import { useCallback, useEffect, useState } from "react";
+
 export const TweetButton: React.VFC = () => {
+  const onClick = useCallback(() => {
+    const url = `https://twitter.com/intent/tweet?url=${encodeURIComponent(
+      window.location.href
+    )}`;
+    window.open(url, "_blank");
+  }, []);
+
   return (
-    <a
-      className="twitter-share-button"
-      data-size="large"
-      href={`https://twitter.com/intent/tweet?url=${window.location.href}`}
-    >
+    <a className="twitter-share-button" data-size="large" onClick={onClick}>
       Tweet
     </a>
   );
